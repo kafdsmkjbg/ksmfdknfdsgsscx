a3faf629b3ccece56126c77a21bdc6889f76f99f
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-17 08:56:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from tempmail import EMail
+from urlextract import URLExtract
+import time
+import re
+import random
+import string
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 """
 ## Web scraping on Streamlit Cloud with Selenium
@@ -29,8 +43,37 @@ with st.echo():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
-
+    options.add_experimental_option("detach", True)
+    
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.set_window_size(1920, 1080)
+    wait = WebDriverWait(driver, 30)
+    driver.get("https://myco.io/")
+    
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/div/div[2]/span'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="firstName"]'))).send_keys(name)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="lastName"]'))).send_keys(name_2)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="email"]'))).send_keys(email.address)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="userName"]'))).send_keys(name)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="password"]'))).send_keys("zxcasdqwe12!A")
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="confirmPassword"]'))).send_keys("zxcasdqwe12!A")
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="link-checkbox"]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="register-btn"]'))).click()
+    time.sleep(5)
+    msg = email.wait_for_message()
+    myCode = msg.body
+    link = (re.findall(r'(https?://auth.myco.io?.*\?.*?)\s', myCode))[0]
+    link2 = extractor.find_urls(link)[0]
+    driver.get(link2)
+    #time.sleep(8)
+    driver.get("https://myco.io/")
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/nav/div/div[3]/button'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[1]/div/form/div[2]/div[2]/input'))).send_keys(name)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/div/form/div[2]/div[3]/input'))).send_keys("zxcasdqwe12!A")
+    wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[1]/div/form/div[2]/div[4]/button'))).click()
+    time.sleep(8)
+    driver.get("https://myco.io/videohome?v=64e459e5a9addef12597ac3a")
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div/div[1]/div/div[1]/div/video-js/button'))).click()
 
-    st.code(driver.page_source)
+
