9a19a36c4b09d7089f825f1638e1c3640262361c
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-17 08:59:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ with st.echo():
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
     options.add_experimental_option("detach", True)
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     
     driver = get_driver()
     driver.set_window_size(1920, 1080)
