0b611d4f5717ae2cc13f94883623c9108d535d2f
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-18 03:31:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ with st.echo():
 
     options = Options()
     options.add_argument("--disable-gpu")
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_experimental_option("detach", True)
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
